6aa0bb8ce7fef0a019295a3fe7e416797a216205
gurmanbh <gurmanbhatia.93@gmail.com>
2015-08-14 05:47:09 -0400

    added name

--- js/main.js
+++ js/main.js
@@ -1,3 +1,12 @@
 (function(){
-	
+	var status = {}
+
+	$('#submit').on('click',function(){
+		status.headline = $('#heading').val();
+		status.media = $('#media').val();
+		status.link = $('#link').val();
+		status.desc = $('#desc').val();
+		status.text = $('#highlight').val();
+		console.log(status)
+	})
 }).call(this);

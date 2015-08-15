076f9a8f611cabd26ecacd9ff7e5b8c248fd0bc0
gurmanbh <gurmanbhatia.93@gmail.com>
2015-08-15 16:58:08 -0400

    got the hover things to work

--- js/main.js
+++ js/main.js
@@ -1,12 +1,24 @@
 (function(){
 	var status = {}
 
+	var embed_html = $('#embed-tooltip').html();
+	var embed_TF = _.template(embed_html);
+
 	$('#submit').on('click',function(){
-		status.headline = $('#heading').val();
+		status.heading = $('#heading').val();
 		status.media = $('#media').val();
 		status.link = $('#link').val();
-		status.desc = $('#desc').val();
-		status.text = $('#highlight').val();
-		console.log(status)
-	})
+		status.desc = $('#desc').val().trim();
+		status.highlight = $('#highlight').val();
+		status.generated_id = guidGenerator();
+		$('#sample').html(embed_TF(status));
+	});
+
+	function guidGenerator() {
+	    var S4 = function() {
+	       return (((1+Math.random())*0x10000)|0).toString(16).substring(1);
+	    };
+	    return (S4()+S4()+"-"+S4()+"-"+S4()+"-"+S4()+"-"+S4()+S4()+S4());
+	};
+
 }).call(this);
---
+++ js/notable.app.js
@@ -0,0 +1,38 @@
+(function(){
+
+	var tooltip_height,tooltip_width, x, buffery = 10,bufferx = 25;
+
+	$(document).on('mouseover','.notable_highlight', function() { 
+
+		x = $(this).attr('data-which');
+		$('.notable_block[data-which='+x+']').addClass('active');
+		tooltip_height = $('.notable_block[data-which='+x+']').height();
+		tooltip_width = $('.notable_block[data-which='+x+']').width();
+	});
+
+	$(document).on('mousemove','.notable_highlight', function(e) {
+		var pos = $(this).position()
+		var highlight_height = $(this).height()
+		var highlight_width = $(this).width()
+
+		if ((pos.left-tooltip_width/2)<0){
+			$('.notable_block[data-which='+x+']').css({
+		        top: pos.top+highlight_height/2-tooltip_height/2 ,
+		        left: pos.left+highlight_width+bufferx ,
+		        position: 'absolute'
+		    });
+		} else {
+		$('.notable_block[data-which='+x+']').css({
+		        top: pos.top+highlight_height+tooltip_height / 2 - buffery,
+		        left: pos.left-tooltip_width/2 ,
+		        position: 'absolute'
+		    });
+		}
+	});
+
+	$(document).on('mouseout','.notable_highlight', function() { 
+		var e = $(this).attr('data-which');
+		$('.notable_block[data-which='+e+']').removeClass('active');
+	});
+
+}).call(this);
